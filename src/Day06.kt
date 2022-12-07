f43f436de8e0aded2a7d5cfa1e73460259e07f6f
mnajborowski <michal.najborowski@gmail.com>
2022-12-07 09:29:10 +0100

    day 6

---
+++ src/Day06.kt
@@ -0,0 +1,31 @@
+fun main() {
+    fun part1(input: String): Int {
+        input.substring(4, input.length).foldIndexed(ArrayDeque(input.take(4).toList())) { i, acc, e ->
+            if (acc.toSet().size == 4)
+                return i + 4
+            else {
+                acc.removeFirst()
+                acc.addLast(e)
+                acc
+            }
+        }
+        throw Exception("Marker not found")
+    }
+
+    fun part2(input: String): Int {
+        input.substring(14, input.length).foldIndexed(ArrayDeque(input.take(14).toList())) { i, acc, e ->
+            if (acc.toSet().size == 14)
+                return i + 14
+            else {
+                acc.removeFirst()
+                acc.addLast(e)
+                acc
+            }
+        }
+        throw Exception("Marker not found")
+    }
+
+    val input = readInput("Day06")
+    println(part1(input.first()))
+    println(part2(input.first()))
+}
